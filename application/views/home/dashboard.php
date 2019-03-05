ed5fb164b12328c400aaed1b91aeb48813968846
Ram Narayan <ramnarayanjss@gmail.com>
2019-03-05 12:14:01 +0530

    dashboard

--- application/views/home/dashboard.php
+++ application/views/home/dashboard.php
@@ -54,7 +54,7 @@
 							</div>
 						</div>
 					--></div>
-					<div class="row gutter-xs">
+				<!--	<div class="row gutter-xs">
 						<div class="col-xs-12 col-md-6">
 							<div class="card">
 								<div class="card-body">
@@ -92,7 +92,7 @@
 								</div>
 							</div>
 						</div>
-					</div>
+					</div> -->
 					<div class="row gutter-xs">
 	<!--	             <div class="col-md-4">
 							<div class="card">
@@ -169,7 +169,7 @@
 												<button type="button" class="card-action card-reload" title="Reload"></button>
 												<button type="button" class="card-action card-remove" title="Remove"></button>
 											</div>
-											<strong>Traffic Source</strong>
+											<strong>Top 10 Due</strong>
 										</div>
 										<div class="card-body" data-toggle="match-height">
 											<ul class="list-group list-group-divided">
@@ -227,7 +227,7 @@
 											</ul>
 										</div>
 									</div>
-									<div class="card">
+									<!-- <div class="card">
 										<div class="card-body">
 											<div class="card-heading">
 												<strong>Location</strong>
@@ -238,7 +238,7 @@
 												<div data-toggle="vector-map" data-map="world_en" data-background-color="null" data-color="#ffffff" data-enable-zoom="true" data-hover-opacity="0.7" data-selected-color="#777" data-show-tooltip="true" data-scale-colors='["#ed4882", "#df1e4f"]' data-values='{ "us":8167, "cn":6724, "gb":6527, "br":6330, "it":6232, "jp":6035, "ru":5871, "fr":5658, "in":5494, "au":5133, "ca":4379, "de":4034, "kp":4887, "ar":4608, "mx":4018, "tr":2706, "za":2066, "sa":1624, "id":1902, "gd":656, "lb":656, "cm":640, "cz":640, "ke":640, "mr":640, "om":640, "sk":640, "as":623, "be":623, "eg":623, "ma":623, "me":623, "my":623, "nz":623, "tv":623, "ua":623, "dz":607, "fj":607, "er":590, "fm":590, "ie":590, "ml":590, "pw":590, "se":590, "sl":590, "ug":590, "bs":574, "mk":574, "mt":574, "sv":574, "sy":574, "tn":574, "ba":558, "cg":558, "gs":558, "bf":541, "ci":541, "ge":541, "lv":541, "ph":541, "sz":541, "am":525, "bb":525, "iq":525, "af":508, "az":508, "ee":508, "ad":492, "bt":492, "by":492, "ch":492, "et":492, "gh":492, "gy":492, "io":492, "kn":492, "np":492, "so":492, "bi":476, "bz":476, "gm":476, "ki":476, "mw":476, "tg":476, "cd":459, "cl":459, "cv":459, "do":459, "la":459, "sb":459, "st":459, "ck":443, "pg":443, "rs":443, "tl":443, "na":426, "ve":426, "ae":410, "at":410, "kh":410, "lc":410, "lr":410, "sc":410, "tz":410, "uz":410, "bd":394, "bw":394, "gt":394, "jm":394, "pa":394, "pl":394, "tm":394, "tw":394, "fi":377, "ir":377, "ly":377, "sr":377, "ec":361, "ga":361, "mc":361, "mh":361, "mn":361, "bh":344, "gw":344, "sd":344, "sn":344, "to":344, "bn":328, "cr":328, "dm":328, "kw":328, "mg":328, "pe":328, "py":328, "th":328, "bo":312, "hn":312, "hu":312, "ng":312, "no":312, "pt":312, "al":295, "ao":295, "lt":295, "mm":295, "mu":295, "mv":295, "ne":295, "ni":295, "ss":295, "tt":295, "ws":295, "lu":279, "md":279, "si":279, "bg":262, "dk":262, "gn":262, "ht":262, "km":262, "vc":262, "vu":262, "zw":262, "cf":246, "cu":246, "cy":246, "gr":246, "nu":246, "rw":246, "sm":246, "tj":246, "vn":246, "ag":230, "bj":230, "pk":230, "ro":230, "ye":230, "co":213, "hr":213, "il":213, "kz":213, "qa":213, "gq":197, "jo":197, "mz":197, "sg":197, "td":197, "zm":197, "dj":180, "is":180, "kg":180, "lk":180, "nl":180, "nr":180, "uy":180, "es":164, "ls":164 }' style="height: 170px; width: 100%"></div>
 											</div>
 										</div>
-									</div>
+									</div> -->
 								</div>
 								<div class="col-md-6">
 									<div class="card">
@@ -248,7 +248,7 @@
 												<button type="button" class="card-action card-reload" title="Reload"></button>
 												<button type="button" class="card-action card-remove" title="Remove"></button>
 											</div>
-											<strong>Top Active Pages</strong>
+											<strong>All Branches</strong>
 										</div>
 										<div class="card-body" data-toggle="match-height">
 											<table class="table table-borderless table-middle">
@@ -348,7 +348,7 @@
 										<button type="button" class="card-action card-reload" title="Reload"></button>
 										<button type="button" class="card-action card-remove" title="Remove"></button>
 									</div>
-									<strong>Activity Log</strong>
+									<strong>Premium Due</strong>
 								</div>
 								<div class="card-body">
 									<div class="timeline">
